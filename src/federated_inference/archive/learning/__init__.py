1d03a3eea35ccbed96165625c2705b46ceb55838
Denise-Phi Khuu <65967082+denisekhuu@users.noreply.github.com>
2026-02-05 14:08:53 +0100

    remove archive

--- src/federated_inference/archive/learning/__init__.py
+++
@@ -1,6 +0,0 @@
-from .CL.model.MnistCNN import CentralMNISTModel
-from .SL.model.MnistClientNN import SplitMNISTClientModel
-from .SL.model.MnistServerNN import SplitMNISTServerModel
-from .FL.horizontal.model.MnistCNN import FederatedMNISTModel
-from .FL.vertical.model.MnistClientCNN import FederatedMNISTClientModel
-from .FL.vertical.model.MnistServerCNN import FederatedMNISTServerModel
--- src/federated_inference/archive/learning/configs/torch_config.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-class TorchConfiguration():
-    DEVICE = torch.device("cuda" if torch.cuda.is_available()
-                          else "mps" if torch.backends.mps.is_available()
-                          else "cpu")
